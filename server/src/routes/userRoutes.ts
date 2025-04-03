fa281b05a3e88e11994fc11c5b10d006d4fe121d
Bharadwajreddy1406 <bharadwajreddy146@gmail.com>
2025-04-03 23:34:10 +0530

    Rename profile route to dashboard for consistency

--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -30,6 +30,6 @@ userRoutes.get("/daily-activity", verifyToken, getDailyActivity);
 userRoutes.put("/daily-activity", verifyToken, updateDailyActivity);
 userRoutes.get("/updateStreak", verifyToken, streakUpdate);
 userRoutes.post("/updatePassword", verifyToken, updatePassword);
-userRoutes.get("/profile", verifyToken, getDashboardData);
+userRoutes.get("/dashhboard", verifyToken, getDashboardData);
 userRoutes.put("/addExp", verifyToken, addExp);
 export default userRoutes;
