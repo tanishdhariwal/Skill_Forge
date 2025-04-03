eaeae322e37d5f4c4ea101ed8caae685351a5537
Bharadwajreddy1406 <bharadwajreddy146@gmail.com>
2025-04-03 19:40:40 +0530

    linked analysis

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,7 @@ import { LogiPage } from "./views/LoginPage";
 import { PageNotFound } from "./views/PageNotFound";
 import Dashboard from "./views/Dashboard";
 import Profile from "./views/Profile";
+import { ViewAnalysis } from "./views/ViewAnalysis.tsx";
 import LearningEnvironment from "./views/LearningEnvironment.tsx";
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <Route path="/login" element={<LogiPage />} />
       <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/profile" element={<Profile/>} />
+      < Route path="/analysis" element={<ViewAnalysis />} />
       <Route path="/learn" element={<LearningEnvironment />} />
       <Route path="*" element={<PageNotFound />} />
     </Routes>
