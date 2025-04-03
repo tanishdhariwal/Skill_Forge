b9fab1962bbd5abd68d7e857ddf343206a173a89
capstj <captainstj@gmail.com>
2025-04-03 17:11:00 +0530

    roadmap

--- server/src/routes/roadmapRoutes.ts
+++ server/src/routes/roadmapRoutes.ts
@@ -1,1 +1,13 @@
-console.log("This is Sample Code");
+import { Router } from "express";
+import { verifyToken } from "../utils/token-manager.js";
+import { createStudyPlan, generateStudyPlan, getStudyPlans } from "../controllers/roadmapController.js";
+
+
+const studyPlanRoutes = Router();
+
+
+studyPlanRoutes.post("/studyPlan", verifyToken, createStudyPlan);
+studyPlanRoutes.get("/studyPlan", verifyToken, getStudyPlans);
+studyPlanRoutes.post("/generateStudyPlan", verifyToken, generateStudyPlan);
+
+export default studyPlanRoutes;
